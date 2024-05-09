297bb5db55fbc17518b7930d5d7450542823626f
Chris <Christopherjorge47@icloud.com>
2024-05-09 17:38:50 -0400

    Updated drinks data

--- src/data/Drinks.js
+++ src/data/Drinks.js
@@ -1,23 +1,33 @@
 const Drinks = [
     {
         name: 'water',
-        price: 0.99
+        price: 0.99,
+        image: 'water.png',
+        description: 'A refreshing cold glass of water, comes in still or sparkling'
     },
     {
         name: 'Coca Cola',
-        price: 1.99
+        price: 1.99,
+        image: 'coke.png',
+        description: 'A fresh bottle of coke'
     },
     {
         name: 'Ice Tea',
-        price: 1.99
+        price: 1.99,
+        image: 'icetea.png',
+        description: 'Home made fresh ice tea, comes in sweetened or unsweetened!'
     },
     {
         name: 'Coffee',
-        price: 1.99
+        price: 1.99,
+        image: 'coffee.png',
+        description: 'Freshly brewed coffee'
     },
     {
         name: 'Lemonade',
-        price: 1.99
+        price: 1.99,
+        image: 'lemonade.png',
+        description: 'Freshly squeezed lemonade'
     },
 ];
 
