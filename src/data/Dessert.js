d4944a54f2b1af66a27ac7e9544613509ecdb7c1
Chris <Christopherjorge47@icloud.com>
2024-05-09 17:44:54 -0400

    Modified desserts data

--- src/data/Dessert.js
+++ src/data/Dessert.js
@@ -1,23 +1,33 @@
 const Dessert = [
     {
         name: "Fried Oreos",
-        price: 5.99
+        price: 5.99,
+        image: 'oreos.png',
+        description: 'Golden, fluffy fried Oreos dusted in powdered sugar'
     },
     {
         name: "Ice Cream",
-        price: 2.99
+        price: 2.99,
+        image: 'icecream.png',
+        description: 'Vanilla, Chocolate, Strawberry, Chocolate Chip, Cookies and Cream'
     },
     {
         name: "Cheese Cake Slice",
-        price: 3.99
+        price: 3.99,
+        image: 'cheesecake.png',
+        description: 'Strawberry, Raspberry, Blueberry, regular'
     },
     {
         name: "Jello",
-        price: 0.99
+        price: 0.99,
+        image: 'jello.png',
+        description: "Cherry, Lime, Orange, Strawberry"
     },
     {
         name: "Pie Slice",
-        price: 3.99
+        price: 3.99,
+        image: 'slice.png',
+        description: 'Pumpkin, Cherry, Apple, Strawberry, Blueberry'
     },
 ];
 
